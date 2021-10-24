57fa23ef47b1240e264718e8f292a4dd5321ff2d
stu-ChrisA <cacosta@stu.socsd.org>
2021-10-24 15:22:02 -0400

    Added a lot

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main 
 {
-  public static void main(String[] args) 
+  public static void main(String[] args) throws InterruptedException 
   {
     TextAdventure adventure = new TextAdventure();
     adventure.play();
--- TextAdventure.java
+++ TextAdventure.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Thread;
 
 public class TextAdventure 
 {
@@ -15,7 +16,7 @@ public class TextAdventure
     ourHero = new Player("Bob", 100, 0);
   }
 
-  public void play()
+  public void play() throws InterruptedException
   {
     String input;
     // start of adventure. You can change this if you like
@@ -29,52 +30,25 @@ public class TextAdventure
     ourHero.changeName(input);
     
     // describe the starting situation. Feel free to change this
-    System.out.println("You wake up to find yourself on the edge of a shadowy forest with the sun nearly set. \nYou see what looks like a city in the distance. \nWhat would you like to do?\n \ncity: go towards the \"City\"? \nforest: turn around and re-enter the forest\nnap: go back to sleep\nmountains: Go up the steep mountains to check out the cave\ngrave: enter \"dead mans\" grave\nbeach: go to the beach and explore a \"ship reck\"\nvillage: go into the near by village\n\n " + ourHero.getName() + ": ");
-
+    System.out.println("You wake up to find yourself on the edge of a shadowy forest with the sun nearly set. \nYou see what looks like a city in the distance and you decided to go into the city");
     // get user input and go to the appropriate zone based on their input
-    input = inScanner.nextLine();
-    if(input.equalsIgnoreCase("city"))
-    {
-    	enterZone1();
-    }
-    else if(input.equalsIgnoreCase("forest"))
-    {
-    	enterZone2();
-    }
-    else if(input.equalsIgnoreCase("mountains"))
-    {
-    	enterZone3();
-    }
-    else if(input.equalsIgnoreCase("grave"))
-    {
-    	enterZone4();
-    }
-    else if(input.equalsIgnoreCase("village"))
-    {
-    	enterZone5();
-    }
-    else if(input.equalsIgnoreCase("beach"))
-    {
-    	enterZone6();
-    }
-    else if(input.equalsIgnoreCase("nap"))
-    {
-    	gameEnd();
-    }
+    Thread.sleep(2000);
+    enterZone1();
 
   }
 
-  private void enterZone1()
+  private void enterZone1() throws InterruptedException
   {
+                                                                                                                                                    //City
     // change image
 
     console.setImage("city.png");
 
     // describe the area/situation to the user. 
     // Give them options for choices.
-
-    console.setImage("pumpkintrio.png");
+    Thread.sleep(3000);
     System.out.println("\n" + ourHero.getName() + " Walks into the city and see a wanted poster near a cafe \n it says \"WANTED DEAD OR ALIVE The Three P's \" \n\nDo you want to get this bounty: (y/n): ");
+    console.setImage("pumpkintrio.png");
     String userInpuString = inScanner.nextLine();
 
     // Take action or go to another zone based on their choice
@@ -113,6 +87,7 @@ public class TextAdventure
           {
             ourHero.setHealth();
             System.out.println("Monster Hit!");
+            System.out.println("Current health is: " + ourHero.getHealth());
           }
           else if(ourHero.getHealth() == 0)
           {
@@ -128,14 +103,35 @@ public class TextAdventure
           {
             System.out.println("You got a hit!");
             monsterHealth = monsterHealth - 5;
+            System.out.println("Monster current health is: " + monsterHealth);
+            if(monsterHealth <= 0 )
+            {
+              System.out.println("You've defeated the Three P's. Now it time to return to the cafe and claim your reward");
+              System.out.println("Type \"claim\" to continue\n");
+              String claimS = inScanner.next();
+              if(claimS.equals("claim"))
+              {
+                console.setImage("download.gif");
+                System.out.println("Bartender: \"Wow " + ourHero.getName() + " has done it! You have earned the bounty\"");
+                ourHero.setGold(500);
+                System.out.println("Awesome now you're 500 gold richer!");
+                System.out.println("In the distance from the cafe you see a hooded figure in a alleyway and you decide to follow him\n");
+                console.setImage("Alley.png");
+                System.out.println();
+                Thread.sleep(2000);
+                System.out.println("You exit the cafe to talk to the hooded figure\n Hooded man: \"Well it seems your interested in something\"");
+                Thread.sleep(1000);
+                console.setImage("hooded.png");
+                System.out.println(ourHero.getName() + ": \"Well of course I want to know why you're in a alleyway.\"");
+                System.out.println("Hooded man: \"Well " + ourHero.getName() + " I need some help with something. \n I found something cool at \"dead man's grave\" but it's a little to diffucult for me to get it all by myself\n Would you be able to help me out");
+                System.out.println(ourHero.getName() + ": \"Of course I can\"");
+                enterZone4();
+                break;
+              }
+            }
           }
-          else if(monsterHealth == 0)
-          {
-            break;
-          }
-        }
+        } 
       }
-
     }
     else if(userInpuString.equalsIgnoreCase("n"))
     {
@@ -173,6 +169,7 @@ public class TextAdventure
 
   private void enterZone2()
   {
+                                                                                                                                              //forest
     // change image
     // ADD CODE HERE
 
@@ -187,6 +184,7 @@ public class TextAdventure
 
   private void enterZone3()
   {
+                                                                                                                                            //mountains
     // change image
     // ADD CODE HERE
 
@@ -199,22 +197,60 @@ public class TextAdventure
     
   }
 
-  private void enterZone4()
+  private void enterZone4() throws InterruptedException
   {
+                                                                                                                        /*Graveyard Zone*/
     // change image
+    console.setImage("graveyard.jpg");
     // ADD CODE HERE
 
     // describe the area/situation to the user. 
     // Give them options for choices.
     // ADD CODE HERE
-
-    // Take action or go to another zone based on their choice
-    // ADD CODE HERE
-    
+    System.out.println("You and the hooded man just arrived at the grave yard.");
+    System.out.println(ourHero.getName() + ": \" So what was the thing you found?\nHooded figure: \"Well I saw treasure in this area but it 's guarded by a ghost name \"The Boo\" and the only way to get it is to play a game.\"\n" + ourHero.getName() + ": \"A game you say..?\n Sure ill help you what's the game?");
+    System.out.println("Hooded figure: \"Well " +ourHero.getName() + " it's a guessing game and I'm not really good at those types of games\"\n");
+    Thread.sleep(1000);
+    console.setImage("ghost.png");
+    Thread.sleep(1000);
+    System.out.println("The Boo: \"Muahahahaha who dares come into MY graveyard?\"");
+    System.out.println(ourHero.getName() + ": \"Tis was me I'm here to play your little game. :) ");
+    System.out.println("The Boo: \"Oh really? If you win ill let you get the treasure and tell you where the next turesure is and if I win I take 100 of your gold.\"");
+    System.out.println(ourHero.getName() + ": \"Game on!\"");
+    System.out.println("\nWhen you're ready type \"start\" to start the guessing game");
+    String guessGameStart = inScanner.next();
+    if(guessGameStart.equalsIgnoreCase("start"))
+    {
+      while(true)
+      {
+        int randomnumber = (int)(Math.random()) * (5-1) + 1;
+        int attempts = 0;
+        System.out.println("The Boo: \"Guess a number between 1 and 5 in 10 attemps");
+        int guessNum = inScanner.nextInt();
+        if(guessNum == randomnumber)
+        {
+          System.out.println("The Boo: \"Fine you win the next treasure is located at the beach near the ship reck\"");
+          enterZone6();
+          break;
+        }
+        else if(guessNum != randomnumber)
+        {
+          System.out.println("Nope Try Again!");
+          attempts++;
+          guessNum = inScanner.nextInt();
+        }
+        else if(attempts > 10)
+        {
+          gameEnd();
+          break;
+        }
+      }
+    }
   }
 
   private void enterZone5()
   {
+                                                                                                                                    //village
     // change image
     // ADD CODE HERE
 
@@ -227,8 +263,9 @@ public class TextAdventure
     
   }
 
-  private void enterZone6()
+  private void enterZone6() 
   {
+                                                                                                                                    //Beach
     // change image
     // ADD CODE HERE
 
@@ -241,7 +278,7 @@ public class TextAdventure
     
   }
 
-  private void gameEnd()
+  private void gameEnd() throws InterruptedException
   {
     // ADD CODE HERE
     console.setImage("gameOver.png");
@@ -257,7 +294,7 @@ public class TextAdventure
       inScanner.close();
     }
   }
-  private void gameEndFinal()
+  /*private void gameEndFinal()
   {
     String StartOver2 = inScanner.nextLine();
     // ADD CODE HERE
@@ -273,5 +310,5 @@ public class TextAdventure
       System.out.println("You can close the game now");
       inScanner.close();
     }
-  }
+  }*/
 }
